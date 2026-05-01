17e025347af2579755b77bc164805dda35ce50a7
Francisco Reina <franciscoreinaponce@gmail.com>
2026-05-01 12:17:12 +0100

    chore: initialize project with spring boot 4.0.6 and kotlin 2.2.21

---
+++ src/main/kotlin/com/franciscoreina/reviewinsight/ReviewInsightApiKtApplication.kt
@@ -0,0 +1,11 @@
+package com.franciscoreina.reviewinsight
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReviewInsightApiKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReviewInsightApiKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/franciscoreina/reviewinsight/ReviewInsightApiKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.franciscoreina.reviewinsight
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReviewInsightApiKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
