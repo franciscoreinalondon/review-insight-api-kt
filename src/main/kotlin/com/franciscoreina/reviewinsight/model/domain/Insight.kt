d72e8daaf2d327282dc60a3b5c4b7116ded15300
Francisco Reina <franciscoreinaponce@gmail.com>
2026-05-02 19:22:22 +0100

    fix(service): typo

--- src/main/kotlin/com/franciscoreina/reviewinsight/model/domain/Insight.kt
+++ src/main/kotlin/com/franciscoreina/reviewinsight/model/domain/Insight.kt
@@ -1,3 +1,3 @@
 package com.franciscoreina.reviewinsight.model.domain
 
-data class Insight(val summary: String, val topProblem: List<Problem>)
+data class Insight(val summary: String, val topProblems: List<Problem>)
