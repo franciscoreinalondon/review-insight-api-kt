77c55c0fd3fe51c861b1e4324189d200a3dc5c57
Francisco Reina <franciscoreinaponce@gmail.com>
2026-05-04 14:34:35 +0100

    feat(client): add qualifier for apple web client

--- src/main/kotlin/com/franciscoreina/reviewinsight/client/apple/AppleRssClient.kt
+++ src/main/kotlin/com/franciscoreina/reviewinsight/client/apple/AppleRssClient.kt
@@ -3,11 +3,12 @@ package com.franciscoreina.reviewinsight.client.apple
 import com.franciscoreina.reviewinsight.client.apple.dto.AppleRssResponseDTO
 import com.franciscoreina.reviewinsight.exceptions.ReviewProviderException
 import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 
 @Component
-class AppleRssClient(val webClient: WebClient) {
+class AppleRssClient(@Qualifier("appleWebClient") val webClient: WebClient) {
 
     private val logger = KotlinLogging.logger {}
 
