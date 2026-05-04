cc02809988eae39fac609656931613d521f31823
Francisco Reina <franciscoreinaponce@gmail.com>
2026-05-04 10:07:39 +0100

    refactor(api): clean up slf4j annotation

--- src/main/kotlin/com/franciscoreina/reviewinsight/controller/InsightController.kt
+++ src/main/kotlin/com/franciscoreina/reviewinsight/controller/InsightController.kt
@@ -6,14 +6,12 @@ import com.franciscoreina.reviewinsight.model.mapper.toResponse
 import com.franciscoreina.reviewinsight.service.InsightService
 import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.validation.Valid
-import lombok.extern.slf4j.Slf4j
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@Slf4j
 @RestController
 @RequestMapping("/v1/insight")
 class InsightController(
